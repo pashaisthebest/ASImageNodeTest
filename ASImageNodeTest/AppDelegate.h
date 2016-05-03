8c5482d6ad7244b17bae17bd9e0eeee998c1edd7
Pavel Gurov <pavelgurov@pavels-macbook-pro.local>
2016-05-03 09:00:18 +0300

    Initial Commit

---
+++ ASImageNodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASImageNodeTest
+//
+//  Created by Pavel Gurov on 03/05/16.
+//  Copyright © 2016 Pavel Gurov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
